91772de188654c70e77ff1541f5312b4bc2febc0
Gunjan <gunjanrawat5@gmail.com>
2024-02-28 21:59:08 +0530

    Login updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <img className=' w-48 absolute' src='https://png.pngtree.com/png-clipart/20190604/original/pngtree-creative-company-logo-png-image_1197025.jpg' alt='Netflix-logo'/>
+    </div>
   )
 }
 
